5212213956a714ce8b4e3a0faaa8541080ca0ca0
tchang421 <hc8ff@virginia.edu>
2020-10-18 12:03:28 -0400

    basic event creation added

--- eventFinder/urls.py
+++ eventFinder/urls.py
@@ -6,5 +6,6 @@ app_name = 'eventFinder'
 urlpatterns = [
     path('', views.index, name='index'),
     path('list/', views.EventListView.as_view(), name='list'),
-    
+    path('create/', views.CreateView.as_view(), name='create'),
+    path('creating/', views.creating, name='creating'),
 ]
