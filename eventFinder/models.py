13026736e66383bbf0bc2287c74724b74c5b0d6d
Simeng Hao <simeng.hao@outlook.com>
2020-10-20 00:31:02 -0400

    add author field to Event model

--- eventFinder/models.py
+++ eventFinder/models.py
@@ -1,10 +1,13 @@
 import datetime
 
 from django.db import models
+from django.db.models.deletion import CASCADE
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Event(models.Model):
     event_name = models.CharField(max_length=200)
     pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, null=True, on_delete=CASCADE)
     def __str__(self):
         return self.event_name
