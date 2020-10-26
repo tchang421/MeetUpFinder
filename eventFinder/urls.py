14605efef490a1b95e3e2ee5169e65779a616504
Simeng Hao <simeng.hao@outlook.com>
2020-10-26 11:24:41 -0400

    update create and list in favor of django conventions

--- eventFinder/urls.py
+++ eventFinder/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'eventFinder'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('new/', views.NewView.as_view(), name='new'),
+    path('create/', views.NewView.as_view(), name='create'),
     path('<int:pk>', views.ShowView.as_view(), name='show'),
     path('<int:pk>/edit', views.UpdateView.as_view(), name='update'),
 ]
--- eventFinder/views.py
+++ eventFinder/views.py
@@ -7,38 +7,35 @@ from django.utils.decorators import method_decorator
 from django.views.generic.base import View
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.views.generic.list import ListView
 
 from .forms import EventForm
 from .models import Event
 
 
-class IndexView(View):
-    def get(self, request, *args, **kwargs):
-        context = {
-            'events':Event.objects.all(),
-        }
-        return render(request,'eventFinder/index.html',context)
-    
-    @method_decorator(login_required)
-    def post(self, request, *args, **kwargs):
-        form = EventForm(request.POST)
-        if form.is_valid():
-            new_event = form.save(commit=False)
-            new_event.author = request.user
-            new_event.save();
-        return redirect(reverse('eventFinder:index'))
+class IndexView(ListView):
+    model = Event
+    context_object_name="events"
+    template_name="eventFinder/index.html"
 
 class NewView(LoginRequiredMixin, CreateView):
     model = Event
     form_class = EventForm
     template_name = 'eventFinder/form.html'
 
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self) -> str:
+        return reverse('eventFinder:show', kwargs={'pk': self.object.id})
+
 class ShowView(DetailView):
     model = Event
     template_name='eventFinder/show.html'
 
 
-class UpdateView(UpdateView):
+class UpdateView(LoginRequiredMixin, UpdateView):
     model = Event
     form_class = EventForm
     template_name = 'eventFinder/form.html'
