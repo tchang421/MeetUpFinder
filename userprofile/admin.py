6c6f80eb2a5931c0aadd56a41fa238c759c4dbee
Simeng Hao <simeng.hao@outlook.com>
2020-11-05 00:31:15 -0500

    add id display for user profile in admin

--- userprofile/admin.py
+++ userprofile/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import UserProfile
 # Register your models here.
-admin.site.register(UserProfile)
+
+class UserProfileAdmin(admin.ModelAdmin):
+    readonly_fields = ('id',)
+
+admin.site.register(UserProfile, UserProfileAdmin)
