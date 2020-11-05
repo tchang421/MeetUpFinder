2a38f20058d42093c6cdbb3824a803d898088d70
tchang421 <hc8ff@virginia.edu>
2020-11-05 13:07:29 -0500

    event description

--- eventFinder/models.py
+++ eventFinder/models.py
@@ -10,8 +10,8 @@ class Event(models.Model):
     event_name = models.CharField(max_length=200)
     event_date = models.DateTimeField(blank=True, null=True)
     pub_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, null=True, on_delete=CASCADE)
     event_description = models.CharField(max_length=1000, default="Generic Event Description")
+    author = models.ForeignKey(User, null=True, on_delete=CASCADE)
 
     def __str__(self):
         return self.event_name
