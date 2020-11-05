b7972fb357eb4df7f90a378ca182f6f4f128d4a4
Simeng Hao <simeng.hao@outlook.com>
2020-11-05 00:30:46 -0500

    upgrade user profile and its display

--- userprofile/models.py
+++ userprofile/models.py
@@ -4,4 +4,11 @@ from django.contrib.auth.models import User
 # Create your models here.
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    display_name = models.CharField(max_length=40, null=True)
+    bio = models.CharField(max_length=200, default='')
     join_date = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.display_name:
+            self.display_name = self.user.username
+        super().save(*args, **kwargs);
