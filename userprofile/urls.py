bdbb515d38c5deb15ec6258c4e6fa919b6a99f8a
Simeng Hao <simeng.hao@outlook.com>
2020-11-17 01:08:59 -0500

    add trailing slashes to various urls

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -5,7 +5,7 @@ app_name = 'userprofile'
 urlpatterns = [
     # path('', views.IndexView.as_view(), name='index'),
     # path('create/', views.NewView.as_view(), name='create'),
-    path('<int:pk>', views.ShowView.as_view(), name='show'),
-    path('<int:pk>/edit', views.UpdateView.as_view(), name='update'),
+    path('<int:pk>/', views.ShowView.as_view(), name='show'),
+    path('<int:pk>/edit/', views.UpdateView.as_view(), name='update'),
     # path('<int:pk>/delete', views.DeleteView.as_view(), name='delete'),
 ]
