5cf532bfa8d52b0774883075a1c45cd3d562c010
Simeng Hao <simeng.hao@outlook.com>
2020-11-23 02:25:13 -0500

    Add attendees list to events

--- eventFinder/models.py
+++ eventFinder/models.py
@@ -5,6 +5,7 @@ from django.db.models.deletion import CASCADE
 from django.urls.base import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
+# from userprofile.models import UserProfile
 from address.models import AddressField
 from geopy.geocoders.googlev3 import GoogleV3
 
@@ -21,14 +22,21 @@ class Event(models.Model):
         decimal_places=7, max_digits=10, default=38.0336)
     longitude = models.DecimalField(
         decimal_places=7, max_digits=10, default=-78.507980)
+    attendees = models.ManyToManyField(User, related_name="events_attended")
     
     def save(self, *args, **kwargs):
+
+        # save coordinates by geocoding address
         geolocator = GoogleV3(api_key='AIzaSyDwMQvVq5I887bnz3zAlz71Onjsq4_PYb0');
         location = None if not self.address else geolocator.geocode(self.address)
         if location:
             self.latitude = location.latitude
             self.longitude = location.longitude
+        
         super().save(*args, **kwargs)
+
+        # save author to attendees
+        self.attendees.add(self.author)
 
     def __str__(self):
         return self.event_name
